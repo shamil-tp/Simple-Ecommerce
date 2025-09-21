e642c2d47163a1824d12134e200e8df095874247
shamil-tp <for.photos.only001@gmail.com>
2025-09-21 10:56:05 +0530

    issue with cookie and middleware

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,24 +1,29 @@
 const Admin = require('../../models/Admin')
 
-exports.adminLoginPage = (req,res)=> res.render('login',{msg:''})
+exports.adminLoginPage = (req, res) => res.render('login', { msg: '' })
 
-exports.adminLogin = async(req,res)=>{
-    let {username,password} = req.body
-    console.log(req.body)
-    if(!(username && password)){
-        return res.render('login',{msg:'Please enter username and password'})
-    }
-    const admin = await Admin.findOne({username:username, password:password})
-    console.log(admin)
-    if(!(admin)){
-        return res.render('login',{msg:"Incorrect Username or Password"})
-    }
-    // return res.render('home')
-    res.cookie('admin',admin,{httpOnly:true}).redirect('/admin') // generate cookie and redirect to next page after proper authentication don't render
+exports.adminLogin = async (req, res) => {
+    try {
+        let { username, password } = req.body
+        console.log(req.body)
+        if (!(username && password)) {
+            return res.render('login', { msg: 'Please enter username and password' })
+        }
+        const admin = await Admin.findOne({ username: username, password: password })
+        console.log(admin)
+        if (!(admin)) {
+            return res.render('login', { msg: "Incorrect Username or Password" })
+        }
+        // return res.render('home')
+        res.cookie('admin', admin, { httpOnly: true }).redirect('/admin') // generate cookie and redirect to next page after proper authentication don't render
 
+    }catch(err){
+        console.log(err)
+        res.redirect('/login')
+    }
 }
 
-exports.adminLogout = (req,res) => res.clearCookie('admin').redirect('/admin/login') // if clearCookie is not working use res.cookie('Admin',null)
+exports.adminLogout = (req, res) => res.clearCookie('admin').redirect('/admin/login') // if clearCookie is not working use res.cookie('Admin',null)
 
 
 
@@ -26,9 +31,9 @@ exports.adminLogout = (req,res) => res.clearCookie('admin').redirect('/admin/log
 
 
 
-    // await Admin.create({
-    //     id:`${Date.now()}`,
-    //     username:'shamil',
-    //     password:'123',
-    //     name:'Shamil T P'
-    // })
+// await Admin.create({
+//     id:`${Date.now()}`,
+//     username:'shamil',
+//     password:'123',
+//     name:'Shamil T P'
+// })
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,15 @@
 
-exports.adminOnly = (req,res,next) => {
-    const admin = req.cookie?.admin
-    if(!admin){
+exports.adminOnly = (req, res, next) => {
+    try {
+        const admin = req.cookie?.admin
+        console.log(admin)
+        console.log('middleware')
+        if (!admin) {
+            return res.redirect('/admin/login')
+        }
+        return next()
+    }catch(err){
+        console.log(err)
         return res.redirect('/admin/login')
     }
-    return next()
 }
